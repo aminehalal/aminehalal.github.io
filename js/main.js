1de87c6482256d044e2d6b9ef7f0d31dbf803ab7
Amine Halal <aminenohalal@gmail.com>
2023-09-22 12:37:13 +0100

    Updating

--- js/main.js
+++ js/main.js
@@ -61,9 +61,9 @@
     if ($('.hero .hero-text h2').length == 1) {
         var typed_strings = $('.hero .hero-text .typed-text').text();
         var typed = new Typed('.hero .hero-text h2', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
+            strings: typed_strings.split('&'),
+            typeSpeed: 90,
+            backSpeed: 18,
             smartBackspace: false,
             loop: true
         });
